f01bed4ce2fa266cb514be4276f02e030c1bfdf1
Eric Ross <epr@rossmbox.com>
2026-09-03 14:23:38 -0700

    Fixed scripts

---
+++ import.sh
@@ -0,0 +1,11 @@
+ollama pull gpt-oss:20b
+ollama pull qwen3:14b
+ollama pull deepseek-r1:14b
+ollama pull deepseek-r1:8b
+ollama pull qwen3:32b
+ollama pull qwen3-coder:30b
+ollama pull qwen3:32b
+ollama pull gemma4:26b
+ollama pull gpt-oss:120b
+ollama pull devstral-small-2:24b
+
