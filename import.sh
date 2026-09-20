bb139fe64431e48d5b20ca2070eae7f5aeaa35d7
Eric Ross <epr@rossmbox.com>
2026-09-20 14:08:45 -0700

    cleaned up models to established open models

--- import.sh
+++ import.sh
@@ -1,31 +1,20 @@
-ollama pull gpt-oss:20b
-ollama pull qwen3:14b
+ollama pull codegemma:7b
+ollama pull codestral
+ollama pull deepseek-coder:6.7b
 ollama pull deepseek-r1:14b
 ollama pull deepseek-r1:8b
-ollama pull qwen3:32b
-ollama pull qwen3-coder:30b
-ollama pull qwen3:32b
-ollama pull qwen3.5:9b
-ollama pull qwen3.5:27b
-ollama pull qwen3.8:27b
+ollama pull devstral-small-2:24b
+ollama pull gemma2:9b
 ollama pull gemma4:26b
 ollama pull gpt-oss:120b
-ollama pull devstral-small-2:24b
-ollama pull qwen2.5-coder:7b
-ollama pull qwen2.5-coder:14b
-ollama pull deepseek-coder:6.7b
-ollama pull codegemma:7b
+ollama pull gpt-oss:20b
 ollama pull llama3.1:8b
 ollama pull mistral:7b
-ollama pull gemma2:9b
-ollama pull codestral
-ollama pull hf.co/unsloth/Qwen3.8-27B-GGUF:UD-IQ2_S
-ollama pull hf.co/unsloth/Qwen3.8-27B-GGUF:UD-IQ2_XXS
-ollama pull hf.co/empero-ai/Qwen3.8-9B-Distill-GGUF:Q4_K_M
-ollama pull hf.co/empero-ai/Qwen3.8-9B-Distill-GGUF:Q5_K_M
-
-
-
-
-
-
+ollama pull qwen2.5-coder:14b
+ollama pull qwen2.5-coder:7b
+ollama pull qwen3.5:9b
+ollama pull qwen3.5:4b
+ollama pull qwen3.5:2b
+ollama pull qwen3:8b
+ollama pull qwen3:14b
+ollama pull qwen3:4b
