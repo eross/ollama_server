dabdf64bf05176933c8a1d0bd31bee197fc370b0
Eric Ross <epr@rossmbox.com>
2026-09-22 09:56:44 -0700

    Cleaned up incompatible models

--- import.sh
+++ import.sh
@@ -1,20 +1,16 @@
-ollama pull codegemma:7b
-ollama pull codestral
-ollama pull deepseek-coder:6.7b
-ollama pull deepseek-r1:14b
-ollama pull deepseek-r1:8b
 ollama pull devstral-small-2:24b
-ollama pull gemma2:9b
 ollama pull gemma4:26b
-ollama pull gpt-oss:120b
 ollama pull gpt-oss:20b
 ollama pull llama3.1:8b
-ollama pull mistral:7b
-ollama pull qwen2.5-coder:14b
-ollama pull qwen2.5-coder:7b
 ollama pull qwen3.5:9b
 ollama pull qwen3.5:4b
 ollama pull qwen3.5:2b
 ollama pull qwen3:8b
 ollama pull qwen3:14b
 ollama pull qwen3:4b
+
+ollama pull hf.co/unsloth/gemma-4-26B-A4B-it-GGUF:UD-IQ2_M
+ollama pull hf.co/unsloth/gemma-4-26B-A4B-it-GGUF:UD-IQ2_XXS
+
+
+
